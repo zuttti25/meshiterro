676f589ad181d54bd78442155d1f68d30e504dd5
zuttti25 <xxx.pxjpp@icloud.com>
2020-12-06 07:18:02 +0000

    画像投稿機能&削除機能を実装しました

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '7aba76d8036b2064be44a24075735b4d972b66449625bb17fed4cd589aaef1b58866824819ff24ef465ba3627e821102fff4452e1418d829f3eecab887134700'
