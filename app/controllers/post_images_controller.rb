ac8ba28957e57d153d3e193b8a2fb01ca392ab67
zuttti25 <xxx.pxjpp@icloud.com>
2020-12-07 00:40:55 +0000

    コメント&いいね機能を実装しましt

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -11,11 +11,12 @@ class PostImagesController < ApplicationController
   end
 
   def index
-    @post_images = PostImage.all
+    @post_images = PostImage.page(params[:page]).reverse_order
   end
 
   def show
     @post_image = PostImage.find(params[:id])
+    @post_comment = PostComment.new
   end
 
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
 devise_for :users
- root to: 'homes#top'
- resources :post_images, only: [:new, :create, :index, :show, :destroy]
+ root 'post_images#index'
+ resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resource :favorites, only: [:create, :destroy]
+    resources :post_comments, only: [:create, :destroy]
+  end
  end
